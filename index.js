9a57b9af9e749a81404a061fe88d99a60105843f
junderw <junderwood@bitcoinbank.co.jp>
2018-07-28 11:21:00 +0900

    Add segwit signatures

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var bs58check = require('bs58check')
+var bech32 = require('bech32')
 var bufferEquals = require('buffer-equals')
 var createHash = require('create-hash')
 var secp256k1 = require('secp256k1')
@@ -14,8 +15,13 @@ function hash160 (buffer) {
   return createHash('ripemd160').update(sha256(buffer)).digest()
 }
 
-function encodeSignature (signature, recovery, compressed) {
-  if (compressed) recovery += 4
+function encodeSignature (signature, recovery, compressed, segwitType) {
+  if (segwitType !== undefined) {
+    recovery += 8
+    if (segwitType === 'bech32') recovery += 4
+  } else {
+    if (compressed) recovery += 4
+  }
   return Buffer.concat([Buffer.alloc(1, recovery + 27), signature])
 }
 
@@ -23,10 +29,11 @@ function decodeSignature (buffer) {
   if (buffer.length !== 65) throw new Error('Invalid signature length')
 
   var flagByte = buffer.readUInt8(0) - 27
-  if (flagByte > 7 || flagByte < 0) throw new Error('Invalid signature parameter')
+  if (flagByte > 15 || flagByte < 0) throw new Error('Invalid signature parameter')
 
   return {
-    compressed: !!(flagByte & 4),
+    compressed: !!(flagByte & 12),
+    segwitType: !(flagByte & 8) ? null : (!(flagByte & 4) ? 'base58' : 'bech32'),
     recovery: flagByte & 3,
     signature: buffer.slice(1)
   }
@@ -44,10 +51,10 @@ function magicHash (message, messagePrefix) {
   return hash256(buffer)
 }
 
-function sign (message, privateKey, compressed, messagePrefix) {
+function sign (message, privateKey, compressed, messagePrefix, segwitType) {
   var hash = magicHash(message, messagePrefix)
   var sigObj = secp256k1.sign(hash, privateKey)
-  return encodeSignature(sigObj.signature, sigObj.recovery, compressed)
+  return encodeSignature(sigObj.signature, sigObj.recovery, compressed, segwitType)
 }
 
 function verify (message, address, signature, messagePrefix) {
@@ -56,9 +63,25 @@ function verify (message, address, signature, messagePrefix) {
   var parsed = decodeSignature(signature)
   var hash = magicHash(message, messagePrefix)
   var publicKey = secp256k1.recover(hash, parsed.signature, parsed.recovery, parsed.compressed)
+  var publicKeyHash = hash160(publicKey)
+  var actual, expected
 
-  var actual = hash160(publicKey)
-  var expected = bs58check.decode(address).slice(1)
+  if (parsed.segwitType) {
+    if (parsed.segwitType === 'base58') {
+      var redeemScript = Buffer.concat([Buffer.from('0014', 'hex'), publicKeyHash])
+      var redeemScriptHash = hash160(redeemScript)
+      actual = redeemScriptHash
+      expected = bs58check.decode(address).slice(1)
+    } else if (parsed.segwitType === 'bech32') {
+      var result = bech32.decode(address)
+      var data = bech32.fromWords(result.words.slice(1))
+      actual = publicKeyHash
+      expected = Buffer.from(data)
+    }
+  } else {
+    actual = publicKeyHash
+    expected = bs58check.decode(address).slice(1)
+  }
 
   return bufferEquals(actual, expected)
 }
